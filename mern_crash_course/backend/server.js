dd06a8674090beca18be25dd3985c04e101083fe
Maghizhan-05 <maghizhankamaraj@gmail.com>
2025-11-01 20:06:51 +0530

    Project completed : Fully functional product store app

--- mern_crash_course/backend/server.js
+++ mern_crash_course/backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import mongoose from "mongoose";
+import path from "path";
 
 import productRoutes from "./routes/product.route.js";
 
@@ -9,9 +10,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 app.use(express.json()); //allows us to accept json data in body
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
